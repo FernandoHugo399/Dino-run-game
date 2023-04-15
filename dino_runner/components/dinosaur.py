30ff58622d4c0a627fc3eabd3336ca2a18157147
Hugo Fernando <Huginho888@outlook.com>
2023-04-15 15:04:20 -0300

    Removendo comentários

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -5,82 +5,77 @@ from pygame.locals import K_UP, K_DOWN
 from pygame.sprite import Sprite
 from dino_runner.utils.constants import DUCKING, RUNNING, JUMPING, JUMP_VELOCITY, X_POSITION_DINO, Y_POSITION_DINO, DEFAULT_TYPE
 
-RUN_IMG = {DEFAULT_TYPE: RUNNING} # Recebe as imagens para animação de corrida
-JUMP_IMG = {DEFAULT_TYPE: JUMPING} # Recebe as imagens para animação de pulo
+RUN_IMG = {DEFAULT_TYPE: RUNNING}
+JUMP_IMG = {DEFAULT_TYPE: JUMPING}
 DUCK_IMG = {DEFAULT_TYPE: DUCKING}
 
-class Dinosaur(Sprite): # São objetos - Objetos da pasta assets
+class Dinosaur(Sprite):
     def __init__(self) -> None:
-        self.type = DEFAULT_TYPE # Define o tipo padrão da imagem
-        self.image = RUN_IMG[self.type][0] # Está pegando a primeira imagem nas constante RUNNING
-        self.dino_rect = self.image.get_rect() # Recebe as medidas do retângulo do dinossauro
-        #print(self.dino_rect.top)
-        #print(self.dino_rect.right)
-        #print(self.dino_rect.bottom)
-        #print(self.dino_rect.left)
-        self.dino_rect.x = X_POSITION_DINO # Posição x do Dino
-        self.dino_rect.y = Y_POSITION_DINO # Posição y do Dino
-        self.step_index = 0 # Índice de passos
-        self.dino_run = True # Variável que define se o Dino está correndo
-        self.dino_down = False # Variável que define se o Dino está abaixado
-        self.dino_jump = False # Variável que define se o Dino está pulando
-        self.jump_vel = JUMP_VELOCITY # Define a velocidade de pulo do dinossauro
+        self.type = DEFAULT_TYPE
+        self.image = RUN_IMG[self.type][0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POSITION_DINO 
+        self.dino_rect.y = Y_POSITION_DINO
+        self.step_index = 0
+        self.dino_run = True
+        self.dino_down = False
+        self.dino_jump = False
+        self.jump_vel = JUMP_VELOCITY
         
     def update(self, user_input):        
-        if self.dino_run: # Caso o Dino esteja correndo
+        if self.dino_run:
             self.run()
             
-        if self.dino_jump: # Caso o Dino pule
+        if self.dino_jump:
             self.jump()
             
-        if self.dino_down: # Caso o Dino esteja abaixado
+        if self.dino_down:
             self.down()
         
-        if user_input[K_UP] and not self.dino_jump and not user_input[K_DOWN]: # Caso a key_up seja clicada
+        if user_input[K_UP] and not self.dino_jump and not user_input[K_DOWN]:
             self.dino_run = False
             self.dino_jump = True  
-        elif user_input[K_DOWN] and not self.dino_jump: # Caso a key_down seja clicada
+        elif user_input[K_DOWN] and not self.dino_jump:
             self.dino_run = False
             self.dino_down = True
             if pygame.key.get_pressed()[K_UP]:
                 self.dino_down = False
                 self.dino_jump = True  
-        elif not self.dino_jump: # Caso não esteja pulando
+        elif not self.dino_jump:
             self.dino_run = True
             self.dino_down = False
             self.dino_jump = False
         
-        if self.step_index >= 10: # Caso o contador de passo chegue a 10
+        if self.step_index >= 10:
             self.step_index = 0
             
-    def draw(self, screen): # Desenha a imagem do dinossauro na tela
+    def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
     
-    def run(self): # Método responsável pela animação de corrida
-        self.image = RUN_IMG[self.type][self.step_index // 5] # Responsável por alternar as imagens
-        self.dino_rect = self.image.get_rect() # Recebe as medidas do retângulo do dinossauro
-        self.dino_rect.x = X_POSITION_DINO # Atribui a posição X do Dino
-        self.dino_rect.y = Y_POSITION_DINO # Atribui a posição Y do Dino
-        self.step_index += 1 # Aumenta o contador dos passos
-        
+    def run(self):
+        self.image = RUN_IMG[self.type][self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POSITION_DINO
+        self.dino_rect.y = Y_POSITION_DINO
+        self.step_index += 1
 
-    def jump(self): # Método responsável pela animação de pulo
-        self.image = JUMP_IMG[self.type] # Recebe a imagem do pulo
-        if self.dino_jump: # Caso o dino esteja pulando
-            self.dino_rect.y -= self.jump_vel * 4 # A imagem do dino vai mudar o eixo Y  # type: ignore
-            if pygame.key.get_pressed()[K_DOWN]: # Desce mais rápido caso seja clicado a key_down
-                self.jump_vel -= 1.6 # Diminui a velocidade do pulo
+    def jump(self):
+        self.image = JUMP_IMG[self.type]
+        if self.dino_jump:
+            self.dino_rect.y -= self.jump_vel * 4 #type: ignore
+            if pygame.key.get_pressed()[K_DOWN]:
+                self.jump_vel -= 1.6
             else:
-                self.jump_vel -= 0.8 # Diminui a velocidade do pulo
+                self.jump_vel -= 0.8
                 
-        if self.dino_rect.y >= Y_POSITION_DINO: # Verifica se já tocou o solo
-            self.dino_rect.y = Y_POSITION_DINO # Volta a posição original do pulo
-            self.dino_jump = False # Desativa o pulo
-            self.jump_vel = JUMP_VELOCITY # Deixa a velocidade pulo padrão
+        if self.dino_rect.y >= Y_POSITION_DINO:
+            self.dino_rect.y = Y_POSITION_DINO
+            self.dino_jump = False
+            self.jump_vel = JUMP_VELOCITY
             
-    def down(self): # Método responsável pela animação de abaixar
-        self.image = DUCK_IMG[self.type][self.step_index // 5] # Responsável por alternar as imagens
-        self.dino_rect = self.image.get_rect() # Recebe as medidas do retângulo do dinossauro
-        self.dino_rect.x = X_POSITION_DINO # Atribui a posição X do Dino
-        self.dino_rect.y = Y_POSITION_DINO + 34 # Atribui a posição Y do Dino
-        self.step_index += 1 # Aumenta o contador dos passos
+    def down(self):
+        self.image = DUCK_IMG[self.type][self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POSITION_DINO
+        self.dino_rect.y = Y_POSITION_DINO + 34
+        self.step_index += 1
--- dino_runner/utils/constants.py
+++ dino_runner/utils/constants.py
@@ -2,79 +2,79 @@ import pygame
 import os
 
 # Global Constants
-TITLE = "Dino Runner" # Título da aplicação
-SCREEN_HEIGHT = 600 # Altura da tela de exibição
-SCREEN_WIDTH = 1100 # Largura da tela de exibição
-FPS = 30 # Frames por segundo
-IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets") # Diretório das imagens
-GAME_SPEED = 20 # Define a velocidade do jogo
+TITLE = "Dino Runner"
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+FPS = 30
+IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
+GAME_SPEED = 20
 
-X_POSITION_BACKGROUND = 0 # Posição eixo x background
-Y_POSITION_BACKGROUND = 380 # Posição eixo y background
-X_POSITION_DINO = 80 # Posição eixo x do dinossauro
-Y_POSITION_DINO = 310 # Posição eixo y do dionossauro
+X_POSITION_BACKGROUND = 0
+Y_POSITION_BACKGROUND = 380
+X_POSITION_DINO = 80
+Y_POSITION_DINO = 310
 
-JUMP_VELOCITY = 8.5 # Velocidade do pulo do dinossauro
+JUMP_VELOCITY = 8.5
 
 # Assets Constants
-ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png")) # Carrega o favicon do dinossauro
+ICON = pygame.image.load(os.path.join(IMG_DIR, "DinoWallpaper.png"))
 
-RUNNING = [ # Carrega as imagens dinossauro enquanto corre
+RUNNING = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
 ]
 
-RUNNING_SHIELD = [ # Carrega as imagens dinossauro enquanto corre e com o escudo ativo
+RUNNING_SHIELD = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun1Shield.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
 ]
 
-RUNNING_HAMMER = [ # Carrega as imagens dinossauro enquant corre e com o martelo ativo
+RUNNING_HAMMER = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Hammer.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoRun2.png")),
 ]
 
-JUMPING = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJump.png")) # Efetua a ação de pular
-JUMPING_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpShield.png")) # Efetua a ação de pular com o escudo
-JUMPING_HAMMER = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpHammer.png")) # Efetua a ação de pular com o martelo
+JUMPING = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJump.png"))
+JUMPING_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpShield.png"))
+JUMPING_HAMMER = pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoJumpHammer.png"))
 
-DUCKING = [ # Carrega as imagens dinossauro enquanto corre abaixado
+DUCKING = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
 ]
 
-DUCKING_SHIELD = [ # Carrega as imagens dinossauro enquanto corre abaixado com o escudo
+DUCKING_SHIELD = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Shield.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
 ]
 
-DUCKING_HAMMER = [ # Carrega as imagens dinossauro enquanto corre abaixado com o martelo
+DUCKING_HAMMER = [
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck1Hammer.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Dino/DinoDuck2.png")),
 ]
 
-SMALL_CACTUS = [ # Carrega as imagenss pequenos cactos
+SMALL_CACTUS = [
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus1.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus2.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/SmallCactus3.png")),
 ]
-LARGE_CACTUS = [ # Carrega as imagenss grandes cactos
+LARGE_CACTUS = [
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus1.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus2.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Cactus/LargeCactus3.png")),
 ]
 
-BIRD = [ # Carrega as imagenss pequenos cactos
+BIRD = [
     pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird1.png")),
     pygame.image.load(os.path.join(IMG_DIR, "Bird/Bird2.png")),
 ]
 
-CLOUD = pygame.image.load(os.path.join(IMG_DIR, 'Other/Cloud.png')) # Carrega a imagem da núvem
-SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png')) # Carrega a imagem do escudo
-HAMMER = pygame.image.load(os.path.join(IMG_DIR, 'Other/hammer.png')) # Carrega a imagem do martelo
+CLOUD = pygame.image.load(os.path.join(IMG_DIR, 'Other/Cloud.png'))
+SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
+HAMMER = pygame.image.load(os.path.join(IMG_DIR, 'Other/hammer.png'))
 
-BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png')) # Carrega o caminho que o dinossauro percorre durante o jogo
+BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png'))
 
-HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png')) # Carrega uma imagem mostrando a vida do dinossauro
+HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png'))
 
 DEFAULT_TYPE = "default"
